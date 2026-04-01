4753401c5c4c951d97cdc03b703b4f9aab94c035
Diego Pedraza <diegopedraza1012@gmail.com>
2026-04-01 09:32:14 -0500

    Traceback for errors

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,3 +1,2 @@
 from .utils import check_difference_config_backup, load_configuration, generate_dropdowns
 from .log import log_file_verification, log_message
-from .compare import compare_program_backup
--- main.py
+++ main.py
@@ -8,4 +8,5 @@ if __name__ == "__main__":
     try:
         Panel(test).root.mainloop()
     except Exception as e:
+        print(traceback.format_exc())
         log_message(traceback.format_exc())
